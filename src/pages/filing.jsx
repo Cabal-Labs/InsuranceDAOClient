36f6f9529822ecb0d1a77760514a3b747320013e
Bryan Kyritz <bryan@securemeeting.org>
2023-09-24 02:50:36 -0400

    updated

--- src/pages/filing.jsx
+++ src/pages/filing.jsx
@@ -1,89 +1,139 @@
 import { Box, Text, Button } from "@chakra-ui/react";
-import * as React from 'react'
+import * as React from "react";
 import {
   usePrepareContractWrite,
   useContractWrite,
   useWaitForTransaction,
-} from 'wagmi'
+} from "wagmi";
 
-import { ethers } from 'ethers'
-
+import { ethers } from "ethers";
+import Treasury from "../contracts/Treasury.json";
 const { utils } = ethers;
 
 export default function Login() {
-  const [hospitalName, setHospitalName] = React.useState('')
-  const [patientAddress, setPatientAddress] = React.useState('')
-  const [medicalProcedure, setMedicalProcedure] = React.useState('')
-  const [medicalProcedureCost, setMedicalProcedureCost] = React.useState('')
+  const [hospitalName, setHospitalName] = React.useState("");
+  const [patientAddress, setPatientAddress] = React.useState("");
+  const [medicalProcedure, setMedicalProcedure] = React.useState("");
+  const [medicalProcedureCost, setMedicalProcedureCost] = React.useState("");
 
   const {
     config,
     error: prepareError,
     isError: isPrepareError,
   } = usePrepareContractWrite({
-    address: '0xFBA3912Ca04dd458c843e2EE08967fC04f3579c2',
-    abi: [
-      {
-        name: 'payHospital',
-        type: 'function',
-        stateMutability: 'nonpayable',
-        inputs: [{ internalType: 'uint32', name: '', type: 'uint32' }],
-        outputs: [],
-      },
+    address: Treasury.address, // Replace with imported address
+    abi: Treasury.abi, // Replace with imported ABI
+    functionName: "payHospital",
+    args: [
+      hospitalName,
+      medicalProcedure,
+      patientAddress,
+      medicalProcedureCost,
     ],
-    functionName: 'payHospital',
-    args: [hospitalName, patientAddress, utils.BigNumber.from(medicalProcedure), utils.BigNumber.from(medicalProcedureCost)],
-  })
-  const { data, error, isError, write } = useContractWrite(config)
+  });
+  const { data, error, isError, write } = useContractWrite(config);
 
   const { isLoading, isSuccess } = useWaitForTransaction({
     hash: data?.hash,
-  })
+  });
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-	console.log("here")
-  }
+    console.log("here");
+  };
 
   return (
     <div className="page">
-      <Box bg="blue.300" maxW={800} className="p-6" borderRadius="6px" width="50%" height="70%">
+      <Box
+        bg="blue.300"
+        maxW={800}
+        className="p-6"
+        borderRadius="6px"
+        width="50%"
+        height="70%"
+      >
         <Text textStyle="subTitle">File a Claim</Text>
         <hr className="my-4" />
-        <form onSubmit={(e) => {
-          e.preventDefault()
-          write?.()
-        }}>
-          <Text color="white" htmlFor="hospitalName">Hospital Name:</Text>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            write?.();
+          }}
+        >
+          <Text color="white" htmlFor="hospitalName">
+            Hospital Name:
+          </Text>
           <input
             id="hospitalName"
             onChange={(e) => setHospitalName(e.target.value)}
             value={hospitalName}
-            style={{ border: '2px solid white', borderRadius: '7px', marginBottom: '1rem', padding: '0.5rem', width: '100%', color: 'white', backgroundColor: 'transparent' }}
+            style={{
+              border: "2px solid white",
+              borderRadius: "7px",
+              marginBottom: "1rem",
+              padding: "0.5rem",
+              width: "100%",
+              color: "white",
+              backgroundColor: "transparent",
+            }}
           />
-          <Text color="white" htmlFor="hospitalName">Patient Address:</Text>
+          <Text color="white" htmlFor="hospitalName">
+            Patient Address:
+          </Text>
           <input
             id="patientAddress"
             onChange={(e) => setPatientAddress(e.target.value)}
             value={patientAddress}
-            style={{ border: '2px solid white', borderRadius: '7px', marginBottom: '1rem', padding: '0.5rem', width: '100%', color: 'white', backgroundColor: 'transparent' }}
+            style={{
+              border: "2px solid white",
+              borderRadius: "7px",
+              marginBottom: "1rem",
+              padding: "0.5rem",
+              width: "100%",
+              color: "white",
+              backgroundColor: "transparent",
+            }}
           />
-          <Text color="white" htmlFor="hospitalName">Medical Procedure:</Text>
+          <Text color="white" htmlFor="hospitalName">
+            Medical Procedure:
+          </Text>
           <input
             id="medicalProcedure"
             onChange={(e) => setMedicalProcedure(e.target.value)}
             value={medicalProcedure}
-            style={{ border: '2px solid white', borderRadius: '7px', marginBottom: '1rem', padding: '0.5rem', width: '100%', color: 'white', backgroundColor: 'transparent' }}
+            style={{
+              border: "2px solid white",
+              borderRadius: "7px",
+              marginBottom: "1rem",
+              padding: "0.5rem",
+              width: "100%",
+              color: "white",
+              backgroundColor: "transparent",
+            }}
           />
-          <Text color="white" htmlFor="hospitalName">Procedure Cost:</Text>
+          <Text color="white" htmlFor="hospitalName">
+            Procedure Cost:
+          </Text>
           <input
             id="medicalProcedureCost"
             onChange={(e) => setMedicalProcedureCost(e.target.value)}
             value={medicalProcedureCost}
-            style={{ border: '2px solid white', borderRadius: '7px', marginBottom: '1rem', padding: '0.5rem', width: '100%', color: 'white', backgroundColor: 'transparent' }}
+            style={{
+              border: "2px solid white",
+              borderRadius: "7px",
+              marginBottom: "1rem",
+              padding: "0.5rem",
+              width: "100%",
+              color: "white",
+              backgroundColor: "transparent",
+            }}
           />
-          <Button marginTop="2%" variant="primary" disabled={!write || isLoading}>
-            {isLoading ? 'Submitting...' : 'Submit'}
+          <Button
+            marginTop="2%"
+            variant="primary"
+            disabled={!write || isLoading}
+          >
+            {isLoading ? "Submitting..." : "Submit"}
           </Button>
           {isSuccess && (
             <div>
@@ -96,9 +146,8 @@ export default function Login() {
           {(isPrepareError || isError) && (
             <div>Error: {(prepareError || error)?.message}</div>
           )}
-
         </form>
       </Box>
     </div>
-  )
+  );
 }
